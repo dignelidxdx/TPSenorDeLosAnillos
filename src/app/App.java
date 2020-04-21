18bb58fe12f758819325b3b51df2a166e129f5e3
dignelidxdx <digneli14@gmail.com>
2020-04-21 12:45:38 -0300

    Metiendo metodo menuInicializacion dentro del main! correr juego!

--- src/app/App.java
+++ src/app/App.java
@@ -19,12 +19,8 @@ public class App {
     public static final String ANSI_WHITE = "\u001B[37m";
     
     public static void main(String[] args) throws Exception {
-
-
-
         
-  
-
+        menuInicializacion();
         
     }
 
